86aaafee1111fce3bacff1c2e31a004448bf9ecc
Ricardo Ponce de León Vargas <Poncerichi4@outlook.com>
2023-09-25 19:49:32 -0600

    insertamos switch para los casos

--- calculadora_git.c
+++ calculadora_git.c
@@ -3,16 +3,59 @@
 
 
 int main(void)
-
-
-
-
 {
 
 
+  int a = 0;
+  int b = 0;
+  char c = 0;
+  int r = 0;
 
-  return 0;
+  printf ("\n\nINTRODUZCA EL PRIMER NUMERO\n");
+  scanf("%d", &a);
+  
+  printf ("\n\nINTRODUZCA EL SEGUNDO NUMERO\n");
+  scanf("%d", &b);
+  
+  printf ("\n\nINTRODUZCA EL CARACTER\n");
+  getchar();
+  scanf("%char", &c);
+  
+  
+  
+  
+  
+  switch(c)
+    {
+      
+    case'+':
+      printf("SUMA");
+      break;
+      
+    case'-':
+      printf("RESTA");
+      break;
+      
+    case'*':
+      printf("MULTIPLICACION");
+      break;
+      
+    case'/':
+      printf("DIVISION");
+      break;
+      
+      
+    default:
+      printf("ERROR");
 
-
-
+      break;
+      
+    }
+  
+  printf("\n\nSU RESPUESTA ES\n: %d",r);
+  
+  
+  getchar();
 }
+
+
